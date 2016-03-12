492a68e4afe53e6089f2e6110c8e3e10cbb06f4d
Varun Gupta <varungupta@Varuns-MacBook-Pro.local>
2016-03-12 23:44:53 +0800

    Upload image

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 var app = angular.module('MovieSearch', []);
 
 app.controller('MainController', function($scope, $http) {
-	$scope.scrape = function() {
+	var fd = new FormData();
+	$scope.moviePic = null;
+	$scope.movieData = null;
+
+	$scope.scrapePic = function() {
 		$http({
 			method: 'POST',
  			url: 'py/data.py',
- 			data: { movieData: $scope.movieData }
+ 			data: fd,
+			headers: {'Content-Type': undefined},
+	    transformRequest: angular.identity
 		}).then(function(data) {
 			console.log(data);
 		});
 	};
-	$scope.movieData = null;
+
+	$scope.uploadFile = function(files) {
+    fd.append("file", files[0]);
+	};
 });
--- py/data.py
+++ py/data.py
@@ -10,9 +10,15 @@ print "Content-type: text/json"
 print
 # print "Content-Type: application/html;charset=utf-8"
 #
-request = json.loads(cgi.FieldStorage().value)
+# request = json.loads(cgi.FieldStorage().value)
+# print(cgi.FieldStorage().value[0].file.read())
+
+filedata = cgi.FieldStorage().value[0]
+if filedata.file:
+    outfile = open("main.jpg", "w")
+    outfile.write(filedata.file.read())
 
 message = scraper(request['movieData'])
 
 # message = json.dumps({'Status': 'Error :('})
-print(message)
+# print(message)
