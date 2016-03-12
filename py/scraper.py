d8799146e0a7c55595b2ccced5968fa062137629
Varun Gupta <varung97@yahoo.co.in>
2016-03-13 03:46:58 +0800

    Bug Fixing Scraper

--- py/scraper.py
+++ py/scraper.py
@@ -2,44 +2,57 @@ from BeautifulSoup import *
 import urllib, json
 
 def imdbscraper(movieData):
-	query = urllib.urlencode ( { 'q' : movieData + " imdb" } )
-	response = urllib.urlopen ('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
+	query = urllib.urlencode({'q' : movieData + " imdb"})
+	jsonObj = None
+	# while jsonObj == None:
+	response = urllib.urlopen('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
 	jsonObj = json.loads(response)
-	result = jsonObj [ 'responseData' ] [ 'results' ]
+		# try:
+	result = jsonObj['responseData']['results']
 	url = result[0]['url']
+		# except:
+		# 	pass
 	r = urllib.urlopen (url).read()
 	soup = BeautifulSoup(r)
-	storyline = soup.find("div", {"itemprop": "description" }).contents[0]
-	name = soup.find("h1", {"itemprop": "name" }).contents[0]
-	releasedate = soup.find("a", {"title": "See more release dates" }).contents[0]
-	runtime = soup.find("time", {"itemprop": "duration"}).contents[0]
-	pgrating = soup.find("meta", {"itemprop": "contentRating"}).contents[0]
-	rating = soup.find("span", {"itemprop": "ratingValue"}).contents[0]
+	storyline = soup.find("div", {"itemprop": "description" }).contents[0].strip()
+	name = soup.find("h1", {"itemprop": "name" }).contents[0].strip()
+	releasedate = soup.find("a", {"title": "See more release dates" }).contents[0].strip()
+	runtime = soup.find("time", {"itemprop": "duration"}).contents[0].strip()
+	pgrating = soup.find("meta", {"itemprop": "contentRating"})['content'].strip()
+	rating = soup.find("span", {"itemprop": "ratingValue"}).contents[0].strip()
 	return {"rating": rating, "pgrating": pgrating, "runtime": runtime,
 	"releasedate": releasedate, "name": name, "storyline": storyline}
 
 def rottentomatoScraper(movieData):
-	query = urllib.urlencode ( { 'q' : movieData + " rotten tomatoes" } )
-	response = urllib.urlopen ('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
+	query = urllib.urlencode ({'q' : movieData + " rotten tomatoes"})
+	jsonObj = None
+	# while jsonObj['responseData'] == None:
+	response = urllib.urlopen('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query ).read()
 	jsonObj = json.loads(response)
-	result = jsonObj [ 'responseData' ] [ 'results' ]
+		# try:
+	result = jsonObj['responseData']['results']
+			# print("Hey")
+		# except:
+		# 	pass
 	url = result[0]['url']
-	r = urllib.urlopen (url).read()
+	r = urllib.urlopen(url).read()
 	soup = BeautifulSoup(r)
 	#add image, cast members, genre, reviews
-	criticConsensus = soup.find("p", {"class": "critic_consensus superPageFontColor"}).contents[0]
-	criticRating = soup.find("span", {'itemprop': "ratingValue"}).contents[0]
-	userRating = soup.find("span", {'itemprop': "ratingValue"}).contents[1]
-	cast = [span.contents[0] for span in soup.find("span", {'itemprop': "name"})[:3]]
-	genres = [span.contents[0] for span in soup.find("span", {'itemprop': "genre"})]
-	image = soup.find("img", {"class": "posterImage"})["src"]
+	criticConsensus = soup.find("p", {"class": "critic_consensus superPageFontColor"}).contents[2].strip()
+	criticRating = soup.find("span", {'itemprop': "ratingValue"}).contents[0].strip()
+	userRating = soup.find("span", {'itemprop': "ratingValue"}).contents[0].strip()
+	director = soup.find("span", {"itemprop": "name"}).contents[0].strip()
+	cast = [span.contents[0].strip() for span in soup.findAll("span", {'itemprop': "name"})[1:4]]
+	genres = [span.contents[0].strip() for span in soup.findAll("span", {'itemprop': "genre"})]
+	image = soup.find("img", {"class": " posterImage"})["src"]
 
 	r = urllib.urlopen(url + "/reviews/?type=top_critics").read()
 	soup = BeautifulSoup(r)
-	reviews = [div.contents[0] for div in soup.find("div", {"class": "the_review"})]
+	reviews = [div.contents[0].strip() for div in soup.findAll("div", {"class": "the_review"})[:6]]
 
 	return {"criticConsensus": criticConsensus, "criticRating": criticRating,
-	"userRating": userRating, "cast": cast, "image": image, "reviews": reviews}
+	"userRating": userRating, "cast": cast, "image": image, "reviews": reviews,
+	"director": director, "genres": genres}
 
 def finalScraper(movieData):
 	imdb = imdbscraper(movieData)
@@ -48,12 +61,12 @@ def finalScraper(movieData):
 			"image": rotten["image"],
 			"ratings": [imdb["rating"], rotten["criticRating"], rotten["userRating"]],
 			"storyline": imdb["storyline"],
-			"cast": rotten["cast"]
-			"genres": rotten["genres"]
+			"cast": rotten["cast"],
+			"genres": rotten["genres"],
 			"runtime": imdb["runtime"],
 			"pgrating" : imdb["pgrating"],
 			"releasedate": imdb["releasedate"],
-			"criticConsensus": imdb["criticConsensus"],
+			"criticConsensus": rotten["criticConsensus"],
 			"reviews": rotten["reviews"]
 		   }
 
