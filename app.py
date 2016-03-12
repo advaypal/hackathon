91fca366e357976ac9c9666d8643f2b4901359ec
Varun Gupta <varung97@yahoo.co.in>
2016-03-13 07:07:03 +0800

    Minor change to app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, os
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/favicon.ico')
 def index():
     return render_template('index.html')
 
