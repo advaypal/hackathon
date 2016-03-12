e8890b9bb20523769326b63665e3da106653355a
Varun Gupta <varung97@yahoo.co.in>
2016-03-13 07:15:35 +0800

    One more minor change to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, os
 
 app = Flask(__name__)
 
@@ -11,4 +11,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
