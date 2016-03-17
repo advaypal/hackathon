e614cf60450b7b178f42f80d672e29b7f749a479
Varun Gupta <varung97@yahoo.co.in>
2016-03-17 22:42:22 +0800

    Splinter

--- image_handler.py
+++ image_handler.py
@@ -5,6 +5,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from PIL import Image
+from splinter import Browser
 
 def image_crop():
     main = Image.open("main.jpg")
@@ -15,12 +16,19 @@ def image_crop():
     main.crop(0, 0, 3 * size[0] / 4, size[1]).save("crop1.jpg")
 
 def image_search(image_url):
-    driver = webdriver.PhantomJS()
-    driver.set_window_size(1120, 550)
-    driver.get("https://www.google.com/searchbyimage?&image_url=" + image_url)
-    guess = driver.find_element_by_class_name("_gUb").text
-    driver.close()
-    return guess
+    # driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
+    # driver.set_window_size(1280, 1024)
+    # driver.get("https://www.google.com/searchbyimage?&image_url=" + image_url)
+    # driver.save_screenshot('image.jpg')
+    # print('_gUb' in driver.page_source)
+    # guess = driver.find_element_by_class_name("_gUb").text
+    # driver.close()
+    # return guess
+    browser = Browser('chrome')
+    browser.visit("https://www.google.com/searchbyimage?&image_url=" + image_url)
+    print('_gUb' in browser.html)
+    guess = browser.find_by_css('._gUb').value
+    print(guess)
 
 def get_guess():
     image_crop()
@@ -33,3 +41,5 @@ def get_guess():
     counts = [guesses.count(guess) for guess in guesses]
     return guesses[count.index(max(count))]
     return image_search("")
+
+image_search("http://assets.flicks.co.nz/images/movies/poster/49/494ba9ff03bdad881378a6fd4092a6c7_500x735.jpg")
